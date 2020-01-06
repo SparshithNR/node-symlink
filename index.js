10e151e0d12b00d0c0ba00602dd7e1424d8471db
sparshithNR <nairbali@usc.edu>
2020-01-06 14:46:04 -0800

    resetting to default

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 const fs = require('fs');
 
-console.log(`----------------TEST-1-------------`);
 console.log(`Before symlink: ${fs.existsSync('linkHere')}`);
 fs.symlinkSync('test/foo.js', 'linkHere');
 console.log(`After symlink: ${fs.existsSync('linkHere')}`);
 console.log(`issymlink: ${fs.lstatSync('linkHere').isSymbolicLink()}`);
 //cleanup
 fs.unlinkSync('linkHere');
-
-console.log(`----------------TEST-2-------------`);
-fs.mkdirSync("tmp");
-fs.symlinkSync(".", "tmp/out");
-console.log(`fs.exist_1 ${fs.existsSync('tmp/out')}`);
-fs.rmdirSync('tmp/out');
