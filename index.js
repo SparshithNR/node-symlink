f7508e6d47695ee7f4bd5b0d60b0e5ffae88695b
sparshithNR <nairbali@usc.edu>
2020-01-07 09:04:44 -0800

    pass dir

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
 fs.mkdirSync("tmp");
-fs.symlinkSync(".", "tmp/out");
+fs.symlinkSync(".", "tmp/out", 'dir');
 console.log(`fs.existSync(tmp/out) : ${fs.existsSync('tmp/out')}`);
 fs.rmdirSync('tmp/out');
