6aae7b87fbe189428a96fae92a41030940f1dd25
sparshithNR <nairbali@usc.edu>
2020-01-09 16:07:51 -0800

    Try catch and junction

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const fs = require('fs');
 
-fs.mkdirSync("tmp");
-fs.symlinkSync(".", "tmp/out", 'junction');
-console.log(`fs.existSync(tmp/out) : ${fs.existsSync('tmp/out')}`);
-fs.rmdirSync('tmp/out');
+fs.mkdirSync('tmp');
+fs.mkdirSync('linkhere');
+fs.symlinkSync('linkhere', 'tmp/out');
+
+let err = false;
+try {
+  fs.rmdirSync('tmp/out');
+} catch (e) {
+  if (e && e.code == 'ENOTDIR') {
+    console.log(`Error caught`);
+    err = true;
+  }
+}
+if (!err) {
+  console.log(`Trying junction`);
+  fs.unlinkSync('tmp/out');
+  fs.symlinkSync('.', 'tmp/out', 'junction');
+  fs.rmdirSync('tmp/out');
+  throw new Error(`Shouldn't reach here`);
+}
